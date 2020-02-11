5cf73161e70693d8be0cb1a8c278857ca6521ce1
Joe Palala <jose.palala@live.com>
2020-02-11 23:32:54 +0800

    install html5 bp

---
+++ category/php5.md
@@ -0,0 +1,11 @@
+TOC
+
+<ul>
+  <li>ypClick here</li>
+  <li>Another topic/li>
+  <li>Security</li>
+  <li>Database</li>
+  <li>Stuf</li>
+</ul>
+
+------
---
+++ index.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+  <meta charset="utf-8">
+  <title>Phptricks.dev</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link rel="manifest" href="site.webmanifest">
+  <link rel="apple-touch-icon" href="icon.png">
+  <!-- Place favicon.ico in the root directory -->
+
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/main.css">
+
+  <meta name="theme-color" content="#fafafa">
+</head>
+
+<body>
+  <!--[if IE]>
+    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+  <![endif]-->
+  <a href="<?php echo "#"; ?>/category/php5" class="button">PHP 5.x</a>
+<a href="#php7" class="button">PHP 7.x</a>
+<a href="#php7" class="button">Laravel</a>
+<a href="#php7" class="button">Symfony</a>
+<a href="#php7" class="button">Packagist</a>
+  <!-- Add your site or application content here -->
+  <p>Just a bunch of tricks for PHP developers.</p>
+
+  <script src="js/vendor/modernizr-3.7.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.slim.min.js" integrity="sha256-pasqAKBDmFT4eHoN2ndd6lN370kFiGUFyTiUHWhU7k8=" crossorigin="anonymous"></script>
+  <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.4.1.min.js"><\/script>')</script>
+  <script src="js/plugins.js"></script>
+  <script src="js/main.js"></script>
+
+  <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+  <script>
+    window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
+    ga('create', 'UA-XXXXX-Y', 'auto'); ga('set','transport','beacon'); ga('send', 'pageview')
+  </script>
+  <script src="https://www.google-analytics.com/analytics.js" async></script>
+</body>
+
+</html>
